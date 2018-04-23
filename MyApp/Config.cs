0a97445546932422ab13863e1f29c3defda39fb5
Minh Anh Trinh <minhanh.xiu@gmail.com>
2018-04-23 14:17:52 -0400

    add config class

--- MyApp/Config.cs
+++ MyApp/Config.cs
@@ -1,13 +1,141 @@
 ﻿using System;
+
 namespace MyApp
 {
     public class Config
     {
-        
+
+        public static int maxHeight;
+        public static int maxWidth;
+        public static int period;
+        public static int dotWidth;
+        public static int bodyElements;
+        public static int snakeSpeed;
+        public static int score;
+        public static int highScore;
+        public static bool gameOver;
 
         public Config()
         {
-            
+            maxWidth = 0;
+            maxHeight = 0;
+            period = 800;
+            dotWidth = 20;
+            bodyElements = 5;
+            snakeSpeed = 5;
+            score = 0;
+            highScore = 0;
+            gameOver = false;
+
         }
+
+        public static int MaxHeight
+        {
+            get
+            {
+                return maxHeight;
+            }
+            set
+            {
+                maxHeight = value;
+            }
+        }
+
+        public static int MaxWidth
+        {
+            get
+            {
+                return maxWidth;
+            }
+            set
+            {
+                maxWidth = value;
+            }
+        }
+
+        public static int Period
+        {
+            get
+            {
+                return period;
+            }
+            set
+            {
+                period = value;
+            }
+        }
+
+        public static int DotWidth
+        {
+            get
+            {
+                return dotWidth;
+            }
+            set
+            {
+                dotWidth = value;
+            }
+        }
+
+        public static int BodyElements
+        {
+            get
+            {
+                return bodyElements;
+            }
+            set
+            {
+                bodyElements = value;
+            }
+        }
+
+        public static int SnakeSpeed
+        {
+            get
+            {
+                return snakeSpeed;
+            }
+            set
+            {
+                snakeSpeed = value;
+            }
+        }
+
+        public static int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                score = value;
+            }
+        }
+
+        public static int HighScore
+        {
+            get
+            {
+                return highScore;
+            }
+            set
+            {
+                highScore = value;
+            }
+        }
+
+        public static bool GameOver
+        {
+            get
+            {
+                return gameOver;
+            }
+            set
+            {
+                gameOver = value;
+            }
+        }
+
     }
 }
--- MyApp/MyCanvas.cs
+++ MyApp/MyCanvas.cs
@@ -22,37 +22,31 @@ namespace MyApp
         Snake snake;
         Dot snakeHead;
         Dot berry;
-        public int maxHeight;
-        public int maxWidth;
-        public int dotWidth = 20;
-        public int bodyElements;
-        public int snakeSpeed;
-        public int score;
-        public int highScore = 0;
-        public bool gameOver = false;
 
         public MyCanvas()
         {
             InitializeComponent();
-            maxHeight = this.ClientSize.Height; // height = 600
-            maxWidth = this.ClientSize.Width; // width = 700
+            new Config();
+
+            Config.MaxHeight = this.ClientSize.Height; // height = 600
+            Config.MaxWidth = this.ClientSize.Width; // width = 700
             this.KeyPreview = true;
             StartNewGame();
 
             // Create 2 timers to update snake & update game status
-            timer.Interval = 1000 / snakeSpeed;
+            timer.Interval = Config.Period / Config.SnakeSpeed;
             timer.Tick += this.UpdateGameSession;
             timer.Start();
-            timer2.Interval = 1000 / snakeSpeed;
+            timer2.Interval = Config.Period / Config.SnakeSpeed;
             timer2.Tick += this.UpdateGameStatus;
             timer2.Start();
         }
 
         // Reset game to initial state
         private void StartNewGame() {
-            snakeSpeed = 5;
-            score = 0;
-            bodyElements = 5;
+            Config.SnakeSpeed = 5;
+            Config.Score = 0;
+            Config.BodyElements = 5;
             CreateSnake();
             CreateBerry();
         }
@@ -60,38 +54,38 @@ namespace MyApp
         // Create snake head and snake body elements
         private void CreateSnake() {
             Point p = new Point(100, 100);
-            snakeHead = new Dot(p, Color.Brown, dotWidth);
+            snakeHead = new Dot(p, Color.Brown, Config.DotWidth);
             snake = new Snake();
             snake.Body.Add(snakeHead);
             Directions snakeDirection = snake.Direction;
 
-            for (int i = 1; i < bodyElements; i++)
+            for (int i = 1; i < Config.BodyElements; i++)
             {
                 int x;
                 int y;
                 if (snakeDirection == Directions.Up)
                 {
                     x = p.X;
-                    y = (p.Y + i * dotWidth);
+                    y = (p.Y + i * Config.DotWidth);
                 }
                 else if (snakeDirection == Directions.Down)
                 {
                     x = p.X;
-                    y = (p.Y - i * dotWidth);
+                    y = (p.Y - i * Config.DotWidth);
                 }
                 else if (snakeDirection == Directions.Left)
                 {
-                    x = p.X + i * dotWidth;
+                    x = p.X + i * Config.DotWidth;
                     y = p.Y;
                 }
                 else
                 {
-                    x = p.X - i * dotWidth;
+                    x = p.X - i * Config.DotWidth;
                     y = p.Y;
                 }
 
                 Point bodyPoint = new Point(x, y);
-                Dot dot = new Dot(bodyPoint, Color.Black, dotWidth);
+                Dot dot = new Dot(bodyPoint, Color.Black, Config.DotWidth);
 
                 snake.Body.Add(dot);
             }
@@ -106,36 +100,36 @@ namespace MyApp
             if (snakeDirection == Directions.Up)
             {
                 x = p.X;
-                y = (p.Y + index * dotWidth);
+                y = (p.Y + index * Config.DotWidth);
             }
             else if (snakeDirection == Directions.Down)
             {
                 x = p.X;
-                y = (p.Y - index * dotWidth);
+                y = (p.Y - index * Config.DotWidth);
             }
             else if (snakeDirection == Directions.Left)
             {
-                x = p.X + index * dotWidth;
+                x = p.X + index * Config.DotWidth;
                 y = p.Y;
             }
             else
             {
-                x = p.X - index * dotWidth;
+                x = p.X - index * Config.DotWidth;
                 y = p.Y;
             }
 
             Point bodyPoint = new Point(x, y);
-            Dot dot = new Dot(bodyPoint, Color.Black, dotWidth);
+            Dot dot = new Dot(bodyPoint, Color.Black, Config.DotWidth);
             snake.Body.Add(dot);
         }
 
         // Create berry randomly
         private void CreateBerry() {
             Random rnd = new Random();
-            int randomX = rnd.Next(3, maxWidth / 20) * 20;
-            int randomY = rnd.Next(3, maxHeight / 20) * 20;
+            int randomX = rnd.Next(3, Config.MaxWidth / 20) * 20;
+            int randomY = rnd.Next(3, Config.MaxHeight / 20) * 20;
             Point randomPoint = new Point(randomX, randomY);
-            berry = new Dot(randomPoint, Color.HotPink, dotWidth);
+            berry = new Dot(randomPoint, Color.HotPink, Config.DotWidth);
         }
 
         private void UpdateGameSession(object sender, EventArgs e)
@@ -160,48 +154,49 @@ namespace MyApp
 
             if (d == Directions.Right && snake.Direction != Directions.Left)
             {
-                newPoint = new Point(snake.Body[0].Location.X + dotWidth, snake.Body[0].Location.Y);
+                newPoint = new Point(snake.Body[0].Location.X + Config.DotWidth, snake.Body[0].Location.Y);
                 snake.Body[0].Location = newPoint;
                 snake.Direction = d;
             }
             if (d == Directions.Left && snake.Direction != Directions.Right)
             {
-                newPoint = new Point(snake.Body[0].Location.X - dotWidth, snake.Body[0].Location.Y);
+                newPoint = new Point(snake.Body[0].Location.X - Config.DotWidth, snake.Body[0].Location.Y);
                 snake.Body[0].Location = newPoint;
                 snake.Direction = d;
             }
             if (d == Directions.Up && snake.Direction != Directions.Down)
             {
-                newPoint = new Point(snake.Body[0].Location.X, snake.Body[0].Location.Y - dotWidth);
+                newPoint = new Point(snake.Body[0].Location.X, snake.Body[0].Location.Y - Config.DotWidth);
                 snake.Body[0].Location = newPoint;
                 snake.Direction = d;
             }
             if (d == Directions.Down && snake.Direction != Directions.Up)
             {
-                newPoint = new Point(snake.Body[0].Location.X, snake.Body[0].Location.Y + dotWidth);
+                newPoint = new Point(snake.Body[0].Location.X, snake.Body[0].Location.Y + Config.DotWidth);
                 snake.Direction = d;
                 snake.Body[0].Location = newPoint;
             }
 
             // Update labels from each interval
-            label1.Text = "Score: " + score;
-            label2.Text = "Level: " + (snakeSpeed - 4);
-            label3.Text = "High Score: " + highScore;
+            label1.Text = "Score: " + Config.Score;
+            label2.Text = "Level: " + (Config.SnakeSpeed - 4);
+            label3.Text = "High Score: " + Config.HighScore;
         }
 
-        // Update score, speed, bodyElements and Canvas regarding the status
-        private void UpdateGameStatus(object sender, EventArgs e) {
+        // Update Score, speed, BodyElements and Canvas regarding the status
+        private void UpdateGameStatus(object sender, EventArgs e)
+        {
             int headX = snake.Body[0].Location.X;
             int headY = snake.Body[0].Location.Y;
 
             // 1. Check if snake has eaten a berry
             // Compare head's coordinates with berry's coordinates
             if (headX == berry.Location.X && headY == berry.Location.Y) {
-                score += 10;
+                Config.Score += 10;
                 CheckHighScore();
                 TrackSpeed();
-                bodyElements++;
-                AddBodyPart(bodyElements - 1);
+                Config.BodyElements++;
+                AddBodyPart(Config.BodyElements - 1);
                 CreateBerry();
                 this.Refresh();
             }
@@ -219,7 +214,7 @@ namespace MyApp
 
             // 3. Check if snake has hit the wall
             // Compare head's coordinates with the wall's coordinates 
-            if (headX > maxWidth || headY > maxHeight || headX < 0 || headY < 0) {
+            if (headX > Config.MaxWidth || headY > Config.MaxHeight || headX < 0 || headY < 0) {
                 CheckHighScore();
                 StartNewGame();
                 TrackSpeed();
@@ -228,25 +223,25 @@ namespace MyApp
         }
 
         private void CheckHighScore() {
-            if (highScore == 0 || highScore < score) {
-                highScore = score;
+            if (Config.HighScore == 0 || Config.HighScore < Config.Score) {
+                Config.HighScore = Config.Score;
             }
         }
 
-        // Keep track of speed regarding the score
+        // Keep track of speed regarding the Score
         private void TrackSpeed() {
             // Increase speed every 30 points
-            if (score % 30 == 0 && score != 0)
+            if (Config.Score % 30 == 0 && Config.Score != 0)
             {
-                snakeSpeed++;
-                timer.Interval = 1000 / snakeSpeed;
-                timer2.Interval = 1000 / snakeSpeed;
+                Config.SnakeSpeed++;
+                timer.Interval = Config.Period / Config.SnakeSpeed;
+                timer2.Interval = Config.Period / Config.SnakeSpeed;
             }
             // Set speed back to initial state
-            if (score == 0) {
-                snakeSpeed = 5;
-                timer.Interval = 1000 / snakeSpeed;
-                timer2.Interval = 1000 / snakeSpeed;
+            if (Config.Score == 0) {
+                Config.SnakeSpeed = 5;
+                timer.Interval = Config.Period / Config.SnakeSpeed;
+                timer2.Interval = Config.Period / Config.SnakeSpeed;
             }
         }
 
@@ -279,13 +274,13 @@ namespace MyApp
         {
             // Draw border 
             Pen borderPen = new Pen(Color.Black, 3);
-            Rectangle border = new Rectangle(1, 1, maxWidth, maxHeight);
+            Rectangle border = new Rectangle(1, 1, Config.MaxWidth, Config.MaxHeight);
             SolidBrush borderBrush = new SolidBrush(Color.White);
             e.Graphics.DrawRectangle(borderPen, border);
             e.Graphics.FillRectangle(borderBrush, border);
 
             // Draw berry
-            int berryWidth = dotWidth;
+            int berryWidth = Config.DotWidth;
             SolidBrush berryBrush = new SolidBrush(berry.Color);
             Rectangle berrySquare = new Rectangle(berry.Location.X,
                                                   berry.Location.Y,
@@ -297,8 +292,8 @@ namespace MyApp
             SolidBrush headBrush = new SolidBrush(snakeHead.Color);
             Rectangle headSquare = new Rectangle(snakeHead.Location.X,
                                                   snakeHead.Location.Y,
-                                                  dotWidth,
-                                                  dotWidth);
+                                                  Config.DotWidth,
+                                                  Config.DotWidth);
             e.Graphics.FillRectangle(headBrush, headSquare);
 
             // Draw snake body
